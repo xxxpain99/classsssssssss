ec27dcb2b0b1042c1b96924a1a5339045868473d
xxxpain99 <84181647+xxxpain99@users.noreply.github.com>
2021-05-20 14:11:33 +0500

    Add files via upload

---
+++ 12121212121212112.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ConsoleApp9
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+
+			Car transport = new Car("mark");
+			transport.drives();
+			transport.honk();
+
+			Motorcycle transport1 = new Motorcycle("mark");
+			transport1.drives();
+			transport1.honk();
+
+			Airplane transport3 = new Airplane("model");
+			transport3.flying();
+			transport3.lands();
+
+			Train transport4 = new Train("mark");
+			transport.drives();
+			transport.honk();
+
+		   Boat transport5 = new Boat("mark");
+			transport.drives();
+			transport.honk();
+		}
+
+		
+	}
+	
+}
